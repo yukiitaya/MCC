f2d0c788eb4fc936a221803632369f02a31c5fa4
yukiitaya <148566876+yukiitaya@users.noreply.github.com>
2024-06-11 00:24:35 +0900

    Update Single case.R

--- Single case.R
+++ Single case.R
@@ -1,88 +1,114 @@
-### 100(1-alpha)% confidence intervals for cases ###
+### 100(1-alpha)% confidence intervals for single cases ###
 
-m <- 10000  # the number of times to calculate the confidence interval
+m <- 1000000  # the number of times to calculate the confidence interval
 alpha <- 0.05
-n <- c(50,100,500,1000,5000,10000)  # sample size
+n <- c(50,100,500,1000,5000,10000)  # sample sizes
 coverage_prob1 <- numeric(length(n))  # a vector for the simple method
 coverage_prob2 <- numeric(length(n))  # a vector for Fisher's z method
+coverage_prob3 <- numeric(length(n))  # a vector for Fisher's z transformation with the asymptotic variance 1/(n-3)
+
+# Vectors to count the number of undefined cases
+na_count1 <- numeric(length(n))
+na_count2 <- numeric(length(n))
+na_count3 <- numeric(length(n))
 set.seed(2023)
 
-# true value of TP,FP,FN,TN
-tTP <- 0.45
-tFP <- 0.05
-tFN <- 0.05
-tTN <- 1-tTP-tFP-tFN
+# True values of TP, FP, FN, TN
+tTP <- 0.45  # p11
+tFP <- 0.05  # p01
+tFN <- 0.05  # p10
+tTN <- 1 - tTP - tFP - tFN  # p00
 
-# true value of MCC
-tMCC <- (tTP*tTN-tFP*tFN)/sqrt((tTP+tFP)*(tTP+tFN)*(tFP+tTN)*(tFN+tTN))
+# True value of MCC
+tMCC <- (tTP * tTN - tFP * tFN) / sqrt((tTP + tFP) * (tTP + tFN) * (tFP + tTN) * (tFN + tTN))
 tMCC
 
-# the function that generates the limiting variance of p_hat
+# Function to generate the limiting variance of p_hat
 variance_matrix <- function(p) {
-    mat <- -outer(p, p)
-    diag(mat) <- p * (1-p)
-    return(mat)
+  mat <- -outer(p, p)
+  diag(mat) <- p * (1 - p)
+  return(mat)
 }
 
-for(j in 1:length(n)){
-    # data generation
-    data <- rmultinom(m, size=n[j], prob = c(tTP,tFP,tFN,tTN))
-    TP <- data[1,]/n[j]
-    FP <- data[2,]/n[j]
-    FN <- data[3,]/n[j]
-    TN <- data[4,]/n[j]
+for (j in 1:length(n)) {
+  # Data generation
+  data <- rmultinom(m, size = n[j], prob = c(tTP, tFP, tFN, tTN))
+  p11 <- data[1, ] / n[j]
+  p01 <- data[2, ] / n[j]
+  p10 <- data[3, ] / n[j]
+  p00 <- data[4, ] / n[j]
 
-    # a column of MCC estimates
-    MCC <- (TP*TN-FP*FN)/sqrt((TP+FP)*(TP+FN)*(FP+TN)*(FN+TN))
+  p1. <- p11 + p10
+  p0. <- p01 + p00
+  p.1 <- p11 + p01
+  p.0 <- p10 + p00
+  
+  D <- sqrt(p1.*p0.*p.1*p.0)
+  MCC <- (p11 * p00 - p10 * p01) / D
+  
+  dphi_p11 <- p00/D - (p1.+p.1)/(2*p1.*p.1)*MCC   # differentiation with respect to p11
+  dphi_p01 <- -p10/D - (p.1+p0.)/(2*p.1*p0.)*MCC  # differentiation with respect to p01
+  dphi_p10 <- -p01/D - (p1.+p.0)/(2*p1.*p.0)*MCC  # differentiation with respect to p10
+  dphi_p00 <- p11/D - (p0.+p.0)/(2*p0.*p.0)*MCC   # differentiation with respect to p00
 
-    # calculattion of the differentiation for the function phi
-    denom1 <- 2 * ((TP+FP)*(TP+FN)*(FP+TN)*(FN+TN))^(3/2)   #denominator
-    dphi_TP <- (FP+TN)*(FN+TN)*(2*TP*FP*FN + 2*FP*FN*TN + TP*FN*TN + TP*FP*TN + FP^2*FN + FP*FN^2)/denom1   # differentiation with respect to TP
-    dphi_FP <- -(TP+FN)*(FN+TN)*(2*TP*FN*TN + 2*TP*FP*TN + TP*FP*FN + FP*FN*TN + TP^2*TN + TP*TN^2)/denom1   # differentiation with respect to FP
-    dphi_FN <- -(TP+FP)*(FP+TN)*(2*TP*FP*TN + 2*TP*FN*TN + TP*FP*FN + FP*FN*TN + TP^2*TN + TP*TN^2)/denom1   # differentiation with respect to FN
-    dphi_TN <- (TP+FP)*(TP+FN)*(2*FP*FN*TN + 2*TP*FP*FN + TP*FN*TN + TP*FP*TN + FP^2*FN + FP*FN^2)/denom1   # differentiation with respect to TN
+  # differentiation of phi and the composition of f and phi
+  dphi <- lapply(1:m, function(i) c(dphi_p11[i], dphi_p01[i], dphi_p10[i], dphi_p00[i]))
+  dfphi <- lapply(1:m, function(i) 1/(1-MCC[i]^2)*dphi[[i]])
 
-    # differentiation of the composite function of f and phi
-    denom2 <- 2 * sqrt((TP + FP) * (TP + FN) * (FP + TN) * (FN + TN)) * (FP * FN - TP * TN + sqrt((TP + FP) * (TP + FN) * (FP + TN) * (FN+ TN))) * (-FP * FN + TP * TN + sqrt((TP + FP) * (TP + FN) * (FP + TN) * (FN + TN)))   #denominator
-    dfphi_TP <- ((FP + TN) * (FN + TN) * (2*TP*FP*FN + 2*FP*FN*TN + TP*FN*TN + TP*FP*TN + FP^2*FN + FP*FN^2)) / denom2   # differentiation with respect to TP
-    dfphi_FP <- -((TP + FN) * (FN + TN) * (2*TP*FN*TN + 2*TP*FP*TN + TP*FP*FN + FP*FN*TN + TP^2*TN + TP*TN^2)) / denom2   # differentiation with respect to FP
-    dfphi_FN <- -((TP + FP) * (FP + TN) * (2*TP*FP*TN + 2*TP*FN*TN + TP*FP*FN + FP*FN*TN + TP^2*TN + TP*TN^2)) / denom2   # differentiation with respect to FN
-    dfphi_TN <- ((TP + FP) * (TP + FN) * (2*FP*FN*TN + 2*TP*FP*FN + TP*FN*TN + TP*FP*TN + FP^2*FN + FP*FN^2)) / denom2   # differentiation with respect to TN
+  # Calculate the limiting variance of p_hat
+  sigma <- lapply(1:m, function(i) variance_matrix(data[,i] / n[j]))
 
-    # the limiting variance of p_hat
-    Sigma <-  lapply(1:m, function(i) variance_matrix(data[,i]/n[j]))
+  ## Limiting variance of MCC calculated by the simple method
+  var_simple <- lapply(1:m, function(i) dphi[[i]] %*% sigma[[i]] %*% dphi[[i]] / n[j])
+  var_simple <- unlist(var_simple)
 
-    ## the limiting variance of MCC calculated by simple method
-    Var_simple <- lapply(1:m, function(i) c(dphi_TP[i], dphi_FP[i], dphi_FN[i], dphi_TN[i]) %*% Sigma[[i]] %*% c(dphi_TP[i], dphi_FP[i], dphi_FN[i],dphi_TN[i])/n[j])
-    Var_simple <- unlist(Var_simple)
-    
-    ## Simple method
-    k <- numeric(m)
-    interval1 <- lapply(1:m, function(i) qnorm(1-alpha/2) * sqrt(Var_simple[i]))
-    interval1 <- unlist(interval1)
-    k <- ifelse(MCC-interval1<tMCC & tMCC<MCC+interval1, 1, 0)
-    k <- k[complete.cases(k)]
-    coverage_prob1[j] <- mean(k)
+  ## Simple method
+  k <- numeric(m)
+  interval1 <- lapply(1:m, function(i) qnorm(1 - alpha / 2) * sqrt(var_simple[i]))
+  interval1 <- unlist(interval1)
+  k <- ifelse(MCC - interval1 < tMCC & tMCC < MCC + interval1, 1, 0)
+  na_count1[j] <- sum(is.na(k))
+  k <- k[complete.cases(k)]
+  coverage_prob1[j] <- mean(k)
 
-    ## Fisher's z method
-    z <- (1/2) * log((1+MCC)/(1-MCC))
-    Var_fisher <- lapply(1:m, function(i) c(dfphi_TP[i], dfphi_FP[i], dfphi_FN[i], dfphi_TN[i]) %*% Sigma[[i]] %*% c(dfphi_TP[i], dfphi_FP[i], dfphi_FN[i], dfphi_TN[i]))
-    Var_fisher <- unlist(Var_fisher)
-    interval2 <- lapply(1:m, function(i) qnorm(1-alpha/2) * sqrt(Var_fisher[i]/n[j]))
-    interval2 <- unlist(interval2)
-    zL <- z - interval2
-    zU <- z + interval2
-    L <- (exp(2*zL)-1)/((exp(2*zL)+1))
-    U <- (exp(2*zU)-1)/((exp(2*zU)+1))
-    l <- numeric(m)
-    l <- ifelse(L<tMCC & tMCC<U, 1, 0)
-    l <- l[complete.cases(l)]
-    coverage_prob2[j] <- mean(l)
+  ## Fisher's z method
+  z <- (1 / 2) * log((1 + MCC) / (1 - MCC))
+  var_fisher1 <- lapply(1:m, function(i) dfphi[[i]] %*% sigma[[i]] %*% dfphi[[i]])
+  var_fisher1 <- unlist(var_fisher1)
+  interval2 <- qnorm(1 - alpha / 2) * sqrt(var_fisher1 / n[j])
+  zL1 <- z - interval2
+  zU1 <- z + interval2
+  L1 <- (exp(2 * zL1) - 1) / (exp(2 * zL1) + 1)
+  U1 <- (exp(2 * zU1) - 1) / (exp(2 * zU1) + 1)
+  l1 <- numeric(m)
+  l1 <- ifelse(L1 < tMCC & tMCC < U1, 1, 0)
+  na_count2[j] <- sum(is.na(l1))
+  l1 <- l1[complete.cases(l1)]
+  coverage_prob2[j] <- mean(l1)
+
+  ## Fisher's z transformation with the asymptotic variance 1/(n-3)
+  var_fisher2 <- 1 / (n[j]-3)
+  interval3 <- qnorm(1 - alpha / 2) / sqrt(n[j]-3)
+  zL2 <- z - interval3
+  zU2 <- z + interval3
+  L2 <- (exp(2 * zL2) - 1) / (exp(2 * zL2) + 1)
+  U2 <- (exp(2 * zU2) - 1) / (exp(2 * zU2) + 1)
+  l2 <- numeric(m)
+  l2 <- ifelse(L2 < tMCC & tMCC < U2, 1, 0)
+  na_count3[j] <- sum(is.na(l2))
+  l2 <- l2[complete.cases(l2)]
+  coverage_prob3[j] <- mean(l2)
 }
+
+# Display results
 options(digits = 4)
-result <- data.frame(row.names=c("n=50","n=100","n=500","n=1000","n=5000","n=10000"), Simple_method=coverage_prob1, Fishers_z_method=coverage_prob2)
+result <- data.frame(row.names = c("n=50", "n=100", "n=500", "n=1000", "n=5000", "n=10000"),
+                     Simple_method = coverage_prob1,
+                     na1 = na_count1,
+                     Fishers_z_method = coverage_prob2,
+                     na2 = na_count2,
+                     Fishers_z_trans = coverage_prob3,
+                     na3 = na_count3)
+
 print(paste("P(y=1)=", tTP + tFN, "MCC=", tMCC))
 result
-
-
-
